dd82a4a7bb4fc12ac7736f7f08be2896d07268e6
kserdult <kserdult@gmail.com>
2023-03-03 21:50:06 +0100

    data filling and movie responsivity

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -1,29 +1,30 @@
-import {AiFillHtml5} from 'react-icons/ai';
+import {FaCss3Alt, FaHtml5, FaSass, FaReact, FaUnity, FaGithub} from 'react-icons/fa';
+import {SiMicrosoftoffice, SiTailwindcss, SiCsharp, SiMysql, SiJavascript, SiRedux, SiCisco, SiTypescript} from 'react-icons/si';
 
 export const skills =[
-    { id:0, title: "HTML", IconTitle: AiFillHtml5 },
-    { id: 1, title: "CSS", IconTitle: AiFillHtml5},
-    { id: 2, title: "SASS", IconTitle: AiFillHtml5},
-    { id: 3, title: "TailWindCSS", IconTitle: AiFillHtml5 },
-    { id: 4, title: "C#", IconTitle: AiFillHtml5 },
-    { id: 5, title: "MySQL", IconTitle: AiFillHtml5 },
-    { id: 6, title: "JavaScript", IconTitle: AiFillHtml5 },
-    { id: 7, title: "React", IconTitle: AiFillHtml5 },
-    { id: 8, title: "Redux", IconTitle: AiFillHtml5 },
-    { id: 9, title: "Unity", IconTitle: AiFillHtml5 },
-    { id: 10, title: "Cisco IOS", IconTitle: AiFillHtml5 },
-    { id: 11, title: "GitHub", IconTitle: AiFillHtml5 },
-    { id: 12, title: "TypeScript", IconTitle: AiFillHtml5 }
+    { id:0, title: "HTML", IconTitle: FaHtml5 },
+    { id: 1, title: "CSS", IconTitle: FaCss3Alt},
+    { id: 2, title: "SASS", IconTitle: FaSass},
+    { id: 3, title: "TailWindCSS", IconTitle: SiTailwindcss },
+    { id: 4, title: "C#", IconTitle: SiCsharp },
+    { id: 5, title: "MySQL", IconTitle: SiMysql },
+    { id: 6, title: "JavaScript", IconTitle: SiJavascript },
+    { id: 7, title: "React", IconTitle: FaReact },
+    { id: 8, title: "Redux", IconTitle: SiRedux },
+    { id: 9, title: "Unity", IconTitle: FaUnity },
+    { id: 10, title: "Cisco IOS", IconTitle: SiCisco },
+    { id: 11, title: "GitHub", IconTitle: FaGithub },
+    { id: 12, title: "TypeScript", IconTitle: SiTypescript }
     ]
 
 export const certificates =[
-    {id:0, title: "ECDL", link:"https://www.dszcmechwart.hu", IconTitle: AiFillHtml5},
-    {id:1, title: "Cisco IT Essentials", link:"https://www.netacad.com", IconTitle: AiFillHtml5},
-    {id:2, title: "IT Essentials: PC Hardware and Software", link:"https://www.netacad.com", IconTitle: AiFillHtml5},
-    {id:3, title: "CCNA Routing and Switching: Introduction to Networks", link:"https://www.netacad.com", IconTitle: AiFillHtml5},
-    {id:4, title: "CCNA Routing and Switching: Routing and S,witching Essentials", link:"", IconTitle: AiFillHtml5},
-    {id:5, title: "CCNA Routing and Switching: Scaling Networks,", link:"https://www.netacad.com", IconTitle: AiFillHtml5},
-    {id:6, title: "CCNA Routing and Switching: Connecting Networks", link:"https://www.netacad.com", IconTitle: AiFillHtml5},
-    {id:7, title: "CCNA Security", link:"https://www.netacad.com", IconTitle: AiFillHtml5},
-    {id:8, title: "React and Redux course", link:"https://developedbyed.com/p/the-creative-react-and-redux-course", IconTitle: AiFillHtml5},
+    {id:0, title: "ECDL", link:"https://www.dszcmechwart.hu", IconTitle: SiMicrosoftoffice},
+    {id:1, title: "Cisco IT Essentials", link:"https://www.netacad.com", IconTitle: SiCisco},
+    {id:2, title: "IT Essentials: PC Hardware and Software", link:"https://www.netacad.com", IconTitle: SiCisco},
+    {id:3, title: "CCNA Routing and Switching: Introduction to Networks", link:"https://www.netacad.com", IconTitle: SiCisco},
+    {id:4, title: "CCNA Routing and Switching: Routing and S,witching Essentials", link:"", IconTitle: SiCisco},
+    {id:5, title: "CCNA Routing and Switching: Scaling Networks,", link:"https://www.netacad.com", IconTitle: SiCisco},
+    {id:6, title: "CCNA Routing and Switching: Connecting Networks", link:"https://www.netacad.com", IconTitle: SiCisco},
+    {id:7, title: "CCNA Security", link:"https://www.netacad.com", IconTitle: SiCisco},
+    {id:8, title: "React and Redux course", link:"https://developedbyed.com/p/the-creative-react-and-redux-course", IconTitle: FaReact},
 ]
--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Movie({item}) {
   return (
-    <div className=' h-36 w-60 relative inline-block m-1'>
-      <item.IconTitle className="text-white bg-black w-full h-36 object-cover rounded-md block"/>
+    <div className=' h-36 w-60 relative inline-block m-1 rounded-md'>
+      <div className='flex w-full h-full justify-center items-center'>
+        <item.IconTitle className="text-white rounded-md h-[30vh] w-[30vw] md:h-[30vh] lg:w-[20vw] max-h-[80%] max-w-[80%]"/>
+      </div>
       <div className=' absolute top-0 left-0 w-full h-full opacity-0 hover:bg-black/80 hover:opacity-100 cursor-pointer transition ease-in-out duration-300 rounded-md'>
         <h1 className='text-white text-center flex justify-center items-center h-full'>{item.title}</h1>
       </div>
