7c73e2bffe2be02f5a03cf423655683bbfbe1968
kserdult <kserdult@gmail.com>
2023-03-07 20:50:38 +0100

    responsivity

--- src/Components/MainTop.jsx
+++ src/Components/MainTop.jsx
@@ -13,11 +13,11 @@ const MainTop = () => {
           <img className='-z-10 absolute top-0 max-h-[100%] h-[56.25vw] object-cover w-full' src={Peaky}/>
         <div className=' pt-[15vw] lg:pt-[10vw] pl-8'>
           <div>
-            <img src={PeakyLogo} className=' w-[20%]' />
+            <img src={PeakyLogo} className=' w-[30%] sm:w-[20%]' />
             <p></p>
           </div>
           <div className='flex mt-6'>
-            <button className=' bg-white text-lg font-medium flex flex-row items-center rounded-md py-1 pr-5 pl-3 mr-2 hover:bg-gray-300'><BsFillPlayFill className='text-3xl mr-1'/> Play</button>
+            <button className=' bg-white text-[2vw] font-medium flex flex-row items-center rounded-md py-[0.6vw] pr-[3vw] pl-[1.8vw] mr-2 hover:bg-gray-300'><BsFillPlayFill className='text-[3vw] mr-1'/> Play</button>
             <button onClick={()=>{setMoreInfo(true)}} className=' bg-gray-600 bg-opacity-80 text-white text-md font-medium flex flex-row items-center rounded-md py-1 pr-5 pl-3 hover:bg-gray-700 hover:bg-opacity-70'><AiOutlineInfoCircle className='text-2xl mr-1'/> More Info</button>
           </div>
         </div>
