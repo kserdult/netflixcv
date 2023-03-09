bf5d6318e07df6a0b9e5d6435225a8a42a6e1394
kserdult <kserdult@gmail.com>
2023-03-09 22:56:20 +0100

    more info progress

--- src/Components/MoreInfo.jsx
+++ src/Components/MoreInfo.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
-import {MdCancel} from 'react-icons/md'
+import {MdCancel, MdEmail, MdLocationCity, MdSchool, MdWork} from 'react-icons/md'
+import {BsFillTelephoneFill} from 'react-icons/bs'
 
 const MoreInfo = ({moreInfo, setMoreInfo}) => {
 
   return (
-    <div onClick={()=> setMoreInfo(false)} className={` flex justify-center items-center w-[100vw] h-[100vh] z-50 ${moreInfo? `fixed bg-black/50` : `hidden bg-black/0`} `}>
-        <div className='relative bg-white w-[90vw] sm:w-[80vw] md:w-[70vw] md:h-[95vh] lg:w-[60vw] xl:w-[50vw] h-[90vh] rounded-md'>
+    <div onClick={()=> setMoreInfo(false)} className={` flex justify-center items-center w-[100vw] h-[100vh] z-50 ${moreInfo? `fixed bg-black/70` : `hidden bg-black/0`} `}>
+        <div className='relative bg-[#181818] text-white w-[90vw] sm:w-[80vw] md:w-[70vw] md:h-[95vh] lg:w-[60vw] xl:w-[50vw] h-[90vh] rounded-md'>
         <MdCancel onClick={()=> setMoreInfo(false)} className='absolute top-3 right-3 text-5xl cursor-pointer'/>
-          <h1>Károly Serdült</h1>
-          <ul>
-            <li>30 784 7672</li>
-            <li>kserdult@gmail.com</li>
-            <li>Hungary, Debrecen</li>
+          <h1 className='text-2xl w-full text-center mt-[3vw] sm:mt-[2.5vw] md:mt-[2vw] lg:mt-[1.5vw]'>Károly Serdült</h1>
+          <ul className='ml-[10%] mt-[5vw] sm:mt-[4vw] md:mt-[3vw] lg:mt-[2vw]'>
+            <li className='flex items-center'><BsFillTelephoneFill className=' mr-3'/> 30 784 7672</li>
+            <li className='flex items-center'><MdEmail className=' mr-3'/> kserdult@gmail.com</li>
+            <li className='flex items-center'><MdLocationCity className=' mr-3'/> Hungary, Debrecen</li>
           </ul>
-          <div>
-            <h1>Education</h1>
-            <h2>Mechwart</h2>
-            <p></p>
+          <div className='ml-[6%] mt-[2.5vw] sm:mt-[2vw] md:mt-[1.5vw] lg:mt-[1vw]'>
+            <h1 className='flex items-center font-bold text-xl'><MdSchool className=' mr-3'/> Education</h1>
+            <h2 className='ml-[10%] text-lg'>Mechwart</h2>
+            <p>
+              
+            </p>
           </div>
-          <div>
-            <h1>Work Experience</h1>
-            <h2>Customer Service Representative</h2>
+          <div className='ml-[6%] mt-[2.5vw] sm:mt-[2vw] md:mt-[1.5vw] lg:mt-[1vw]'>
+            <h1 className='flex items-center font-bold text-xl'><MdWork className=' mr-3'/> Work Experience</h1>
+            <h2 className='ml-[10%] text-lg'>Customer Service Representative</h2>
             <p>
               
             </p>
